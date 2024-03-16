e5af3ec3324e17ffcfaa1229f0cd1f7cdfaabf6d
Ayush jha <105988266+Ayushjha9988@users.noreply.github.com>
2024-03-16 06:24:06 +0530

    Update shayari.py

--- DAXXMUSIC/plugins/bot/shayari.py
+++ DAXXMUSIC/plugins/bot/shayari.py
@@ -64,7 +64,7 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/About_EvoXpro_Owner")
+                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/+r2EpqWqO53JkNDM1")
                     
                 ]
             ]
@@ -82,7 +82,7 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/About_EvoXpro_Owner")
+                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/+r2EpqWqO53JkNDM1")
                     
                 ]
             ]
